935afca51eb855eb52608a0aaaf10a6b1860fe4f
Tiz100 <simone.dragon@btinternet.com>
2016-08-12 11:49:43 +0100

    adding the chat

---
+++ IonicChat/platforms/android/assets/www/js/services/current_user.service.js
@@ -0,0 +1,2 @@
+angular.module('webclient')
+.value('currentUser', _.random(1000000).toString());
---
+++ IonicChat/www/js/pubnub.module.js
@@ -0,0 +1,6 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('webclients', []);
+    //angular.module('webclients.menu.chat', []);
+})();
